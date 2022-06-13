ac2d0e6721e7c1e3d91550083f3d0da4c91abbb0
Ayrton Diniz <ayrton.diniz@restoque.com.br>
2022-06-13 17:16:39 -0300

    Atualização

--- Worker.cs
+++ Worker.cs
@@ -19,17 +19,19 @@ public class Worker : BackgroundService
         while (!stoppingToken.IsCancellationRequested)
         {
             HttpStatusCode status = await Requesters.GetStatusFromUrl(_sites.Url);
-            Console.WriteLine("The site is run.");
+            Console.WriteLine("######## =================== ########");
+            Console.WriteLine("######## O site está rodando ########");
+            Console.WriteLine("######## =================== ########");
 
             if (status != HttpStatusCode.OK)
             {
-                string nameFile = string.Format("logfile_{0}.txt", DateTime.Now.ToString("yyyyMMdd"));
+                string nameFile = string.Format("logfile_{0}.txt", DateTime.Now.ToString("ddMMyyyy"));
                 string path = Path.Combine(@"C:\Users\ayrton.diniz\Desktop\Treino .NET\MonitorSite", nameFile);
                 StreamWriter logFile = new StreamWriter(path, true);
                 logFile.WriteLine(string.Format("O Site {0} ficou fora do ar em {1}.", _sites.Url, DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")));
                 logFile.Close();
 
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("Worker rodando em: {time}", DateTimeOffset.Now);
 
             }
 
